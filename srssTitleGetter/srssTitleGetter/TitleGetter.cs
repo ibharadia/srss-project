99704c053d5be450dbf5814c87642a87be3cff2b
ibharadia <ibha2048@gmail.com>
2016-08-24 00:29:08 -0400

    Title Fetching Console Program
    Takes rss feed URL as input asks for a name for output text file and
    writes all feed titles to a new text file by the specified name, one
    title per line.

---
+++ srssTitleGetter/srssTitleGetter/TitleGetter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace srssTitleGetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a URL (Ex. https://www.reddit.com/.rss): ");
+            string url = Console.ReadLine();
+
+            Console.Write("Please specify a name for the output file (Ex. RSSfeed.txt): ");
+            string filename = Console.ReadLine();
+
+            string filepath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            filepath = Path.Combine(filepath, filename);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(url);
+            XmlNodeList elements = doc.GetElementsByTagName("title");
+
+            string[] titles = new string[elements.Count];
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                titles[i] = elements[i].InnerXml;
+            }
+            System.IO.File.WriteAllLines(@filepath, titles);
+        }
+    }
+}
